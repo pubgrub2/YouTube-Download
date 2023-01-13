0262b1a0a6c31d946c6a83763560837f7c1798c2
pubgrub2 <janne@fuechse.de>
2023-01-13 03:27:20 +0100

    seconds

--- downloader.py
+++ downloader.py
@@ -1,7 +1,5 @@
 from pytube import YouTube
-
-def on_progress(bytes_remaining):
-	print(bytes_remaining)
+import ffmpeg
 
 def getVideo(link):
 	biggestVideo = None
@@ -16,15 +14,26 @@ def getVideo(link):
 	print("video size(kb): " + str(biggestVideo.filesize_kb))
 
 	if biggestVideo.includes_audio_track == False:
+		needsCombine = True
 		for x in streamAudio:
 			if biggestAudio == None or x.filesize_kb > biggestAudio.filesize_kb:
 				biggestAudio = x
 		print("audio size(kb): " + str(biggestAudio.filesize_kb))
-		audioPath = biggestAudio.download(filename_prefix = "audio_")
-	videoPath = biggestVideo.download(filename_prefix = "video_")
+		audioPath = biggestAudio.download(output_path = "temp/", filename_prefix = "audio_")
+	videoPath = biggestVideo.download(output_path = "temp/", filename_prefix = "video_")
 
-	print(biggestVideo)
-	print(biggestAudio)
+	if needsCombine == True:
+		textlist = open("temp/list.txt")
+		textlist.write("file \"" + str(videoPath) + "\"\n" + "file \"" + str(audioPath) + "\"")
+		# input_video = ffmpeg.input(videoPath)
+		# input_audio = ffmpeg.input(audioPath)
+		(
+			ffmpeg
+			.input("temp/list.txt", format="concat", safe=0)
+			.output("output/" + biggestVideo.default_filename, c="copy")
+			.run()
+		)
+		# ffmpeg.concat(input_video, input_audio, v=1, a=1).output("output/" + biggestVideo.default_filename, c="copy").run()
 
 getVideo("https://www.youtube.com/watch?v=tPZRdUnV2vg")
 exit()
